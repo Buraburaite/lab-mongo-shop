7383fadfaf001d38bbb81a8330e19042c4d35dd4
Javier <jvrcueto@outlook.com>
2017-02-21 16:14:15 -0500

    working on new document

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ MongoClient.connect(dbUrl, (err, db) => {
     return;
   }
 
-  console.log('Hello World!');
-  db.close();
+  const newReview = {
+    author: 'Nizar',
+    rating: 5,
+    restaurant: 'Wendy\'s',
+    comment: 'Good bang for your buck.'
+  };
+
+  db.collection('reviews').insertOne(newReview, (err, newDoc) => {
+    if (err) {
+      console.log('well, shit');
+      console.log(err);
+      return;
+    }
+
+    console.log('insertOne Success');
+  });
+
+    console.log('Cnxt established...');
+  // db.close();
 });
